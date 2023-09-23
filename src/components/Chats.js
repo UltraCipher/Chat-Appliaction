065f46c2010a3a96377c67284025be483b34253e
Mahiya Hoseinpour <mahiyahp3@gmail.com>
2023-09-23 19:35:44 +0330

    first commit

---
+++ src/components/Chats.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { auth } from '../firebase';
+import { useHistory } from 'react-router-dom';
+import { ChatEngine } from "react-chat-engine";
+import axios from 'axios';
+
+// Components
+import Navbar from './Navbar';
+
+// Styles
+import styles from "./Chats.module.css"
+
+// Context
+import { AuthContext } from "../contexts/AuthContextProvider";
+
+const Chats = () => {
+
+    const [loading, setLoading] = useState(true);
+    const user = useContext(AuthContext);
+    const history = useHistory();
+
+    useEffect(() => {
+        if(!user) {
+            history.push("/");
+            return;
+        }
+
+        axios.get("https://api.chatengine.io/users/me", {
+            headers: {
+                "project-id": "66e9c274-b9c5-47cc-9770-f2df3ad88331",
+                "user-name": user.email,
+                "user-secret": user.uid
+            }
+        })
+        .then(() => {
+            setLoading(false)
+        })
+        .catch(() => {
+            let formdata = new FormData();
+            formdata.append("email", user.email);
+            formdata.append("username", user.email);
+            formdata.append("secret", user.uid);
+            getFile(user.photoURL)
+                .then(avatar => {
+                    formdata.append("avatar", avatar, avatar.name)
+                    axios.post("https://api.chatengine.io/users/", formdata, {
+                        headers: {
+                            "private-key": "606e1dca-f488-44ec-8134-fa703d6a6982"
+                        }
+                    })
+                    .then(() => setLoading(false))
+                    .catch(error => console.log(error))
+                    
+                })
+        })
+
+    }, [user, history])
+
+    const getFile = async (url) => {
+        const response = await fetch(url);
+        const data = await response.blob();
+        return new File([data], "userPhoto.jpg", {type: "image/jpeg"})
+    }
+
+    const logoutHandler = async () => {
+        await auth.signOut();
+        history.push("/")
+    }
+
+    if (!user || loading) return "loading..."
+
+    return (
+        <div className={styles.container}>
+            <Navbar logoutHandler={logoutHandler} />
+
+            <ChatEngine
+                height="calc(100vh - 50px)"
+                projectID="66e9c274-b9c5-47cc-9770-f2df3ad88331"
+                userName={user.email}
+                userSecret={user.uid}
+            />
+        </div>
+    );
+};
+
+export default Chats;
